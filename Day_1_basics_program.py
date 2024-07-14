01159228d4a64e181315eca833e603e963e143ab
Jeevangg22 <Jeevangg80@gmail.com>
2024-07-14 22:33:59 +0530

    added new programs

--- Day_1_basics_program.py
+++ Day_1_basics_program.py
@@ -10,7 +10,6 @@
 # num = [90, 29, 98, 34, 21, 99, 45]
 # print_largest(num)
 
-
 # Print even number in a given range
 # def range_even(begin, last):
 #     for i in range(begin, last):
@@ -20,7 +19,6 @@
 #
 # start, end = map(int, input("enter start and end range: ").split())
 # range_even(start, end)
-
 
 
 # Triangle pattern
@@ -34,7 +32,6 @@
 #
 # start, end = map(int, input("enter start and end range: ").split())
 # pattern(start, end)
-
 
 # square pattern
 # def pattern(begin, last):
@@ -61,3 +58,21 @@
 #     print(f"Palindrome {word} : {word[::-1]}")
 # else:
 #     print(f"NOT an Palindrome {word} : {word[::-1]}")
+
+# Maximum marks in a list
+# Max_marks = [92,78,45,93,67,89,98,67,87]
+# highest = Max_marks[0]
+# for i in Max_marks:
+#     if i > highest:
+#         highest = i
+# print(highest)
+# print(max(Max_marks))
+
+# Finding an frequency of an numbers
+
+# from collections import Counter
+# number = [1,1,2,3,2,1,3,2,1,3,4,5,5,3,2,1,2,1,3]
+# print(Counter(number))
+#
+# str = input("enter string: ")
+# print(Counter(str))
